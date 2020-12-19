f4a1e42b85eceaf0aaeb97bdd1a19bc5db595b41
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2020-12-19 16:23:37 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,5 +2,8 @@
 title CMDTYPER Install
 echo Make sure you have run this as admin! To make custom Commands, my program has to do some work with
 echo System32 and Windows might not allow me to.
+pause
 move $type.bat C:\Windows\System32
-move $makekeyinput.bat C:\Windows\System32 
+echo Done!
+pause
+
