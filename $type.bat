5c0ebec5a33175f50965a7e9e9c029ca8782d76e
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2020-12-19 16:38:09 -0700

    Update $type.bat

--- $type.bat
+++ $type.bat
@@ -1,7 +1,7 @@
 @echo off
 set random=
 set typeid=%random%
-echo set text=
+echo Text To Type:
 set text=
 set /p text=
 echo Enter a vbs instance name - it does not really matter:
@@ -9,8 +9,9 @@ set vbi=
 set /p vbi=
 cd C:\Windows\System32
 echo Set WshShell = WScript.CreateObject("WScript.Shell") >> %vbi%.vbs
-echo. WScript.Sleep 1000 >> %vbi%.vbs
+echo. WScript.Sleep 2000 >> %vbi%.vbs
 echo.WshShell.SendKeys "%text%" >> %vbi%.vbs
 echo Instance created! Linking a command to it. 
 echo @echo off >> $typefromid%typeid%.bat
 echo.start %vbi%.vbs >> $typefromid%typeid%.bat
+echo Your command is: $typefromid%typeid%
