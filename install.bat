232d649bdee5e57eb7f0e232695b7b46fb63c971
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2020-12-19 16:05:36 -0700

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+title CMDTYPER Install
+echo Make sure you have run this as admin! To make custom Commands, my program has to do some work with
+echo System32 and Windows might not allow me to.
+move $type.bat C:\Windows\System32
+move $makekeyinput.bat C:\Windows\System32 
